de6b86bcadd08a60ca999ac2a01de454f97eaf89
Óscar Martorell Giner <oscarmartorellg@gmail.com>
2026-02-11 00:35:14 +0100

    Correccion App.js

--- NuevoResenalo/App.js
+++ NuevoResenalo/App.js
@@ -1,11 +1,21 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import Login from "./src/loginScreen/login";
-import Register from './src/registerScreen/register'
-import Language from './src/Componentes/language/language';
-import Place from './src/placeScreens/Place'
+import Register from "./src/registerScreen/register";
+import Language from "./src/Componentes/language/language";
 const Stack = createStackNavigator();
 const App = () => (
-  <Place/>
+  <NavigationContainer>
+    <Stack.Navigator options="false">
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Login"
+        component={Login}
+      />
+      <Stack.Screen name="Register" component={Register} />
+
+      <Stack.Screen name="Lang" component={Language} />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
 export default App;
