c9dae9df850ce935a03c67602e6f7a0079fb1ba3
Carlos Teran Sanchez <catesa@alumnatflorida.es>
2026-02-10 20:15:05 +0100

    Primera parte i18n

--- NuevoResenalo/App.js
+++ NuevoResenalo/App.js
@@ -1,11 +1,16 @@
-import Main from "./src/Main";
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './src/loginScreen/login'
+import Login from "./src/loginScreen/login";
 import Register from './src/registerScreen/register'
-
+import Language from './src/Componentes/language/language';
 const Stack = createStackNavigator();
 const App = () => (
-  <Main/> 
+  <NavigationContainer>
+    <Stack.Navigator options="false">
+      <Stack.Screen options={{headerShown: false}} name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name='Lang' component={Language} />
+    </Stack.Navigator>
+  </NavigationContainer>
 );
 export default App;
