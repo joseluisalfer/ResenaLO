7c2ab3c153429a494e4a44c91d29c036f246d071
David Serrano <daseta@alumnatflorida.es>
2026-02-11 20:15:12 +0100

    Intento de ejecutar JAR

--- BackEnd/MongoApi/src/main/java/principal/controller/PrincipalController.java
+++ BackEnd/MongoApi/src/main/java/principal/controller/PrincipalController.java
@@ -6,12 +6,10 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
-import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,13 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-
 import principal.model.Comments;
 import principal.model.Review;
 import principal.model.User;
@@ -35,33 +26,28 @@ import principal.repository.CommentsRepository;
 import principal.repository.ReviewRepository;
 import principal.repository.UserRepository;
 
-import static com.mongodb.client.model.Filters.*;
-
 @RestController
 @RequestMapping("first")
 public class PrincipalController {
 
+	@Autowired
+	private CommentsRepository commentsRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ReviewRepository reviewRepository;
+	
+	
 	// Genera la contraseña encriptada
 	public static String hashPassword(String plainPassword) {
 		// 10-12 suele ser un buen coste; 12 es común si el servidor lo aguanta
 		int cost = 12;
 		return BCrypt.hashpw(plainPassword, BCrypt.gensalt(cost));
 	}
+	
 
-	private final UserRepository userRepository;
-	private final CommentsRepository commentsRepository;
-	private final ReviewRepository reviewRepository;
-
-	public PrincipalController(UserRepository userRepository, CommentsRepository commentsRepository,
-			ReviewRepository reviewRepository) {
-		this.userRepository = userRepository;
-		this.commentsRepository = commentsRepository;
-		this.reviewRepository = reviewRepository;
-	}
-
-	MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-	MongoDatabase database = mongoClient.getDatabase("ResenaLo");
-	MongoCollection<Document> accounts = database.getCollection("acounts");
 
 	@PostMapping("/register")
 	public ResponseEntity<Object> register(@RequestParam("user") String user, @RequestParam("password") String password,
